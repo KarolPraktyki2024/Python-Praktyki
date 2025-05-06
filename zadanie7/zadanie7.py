233a08779cd10a96dba5d8106d17651967464a8f
KarolPraktyki2024 <karolpraktyki@op.pl>
2025-05-06 14:59:19 +0200

    Add files via upload

---
+++ zadanie7/zadanie7.py
@@ -0,0 +1,16 @@
+import random
+import time
+
+while(True):
+    myChoice = input('Wybierz stronę monety (o/r) lub 0 aby zakończyć')
+    print('3')
+    time.sleep(1)
+    print('2')
+    time.sleep(1)
+    print('1')
+    time.sleep(1)
+
+    compChoice = random.choice(["orzeł","reszka"])
+
+    
+    
